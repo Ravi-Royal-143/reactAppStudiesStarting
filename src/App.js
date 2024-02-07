7c8fb7ccb3e35fa7f8aee13e382e03bf0ec972f1
Ravi-Royal-143 <raviroyalmech143@gmail.com>
2024-02-07 13:54:15 +0530

    updated the arr value

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import { useState } from 'react';
 import InitialReactApp from './pages/InitialReactApp';
 import { Context } from './state/dummy-context';
 
 function App() {
-  const dummyContext = {
-      dummyData: ['ravi', "preethi"]
-    }
+  const [dummyArr, setDummyArr] = useState(['ravi', 'preethi']);
+  let dummyContext = {
+    dummyData: dummyArr // state variable is used here so the changes got reflected(refresh of app happened)
+  }
+
+  function changeContext() {
+    setDummyArr([
+      ...dummyContext.dummyData,
+      "newText"
+    ]);
+  }
+
   return (
     <Context.Provider value={dummyContext}>
       <div className="App">
         <header className="App-header">
           Test
+          <button onClick={changeContext}>change Context value</button>
           <InitialReactApp dataFromParent="SampleData"></InitialReactApp>
         </header>
       </div>
