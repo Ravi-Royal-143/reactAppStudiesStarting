339a32c599bd15d7772661173da73c55b00b3d9e
Ravi-Royal-143 <raviroyalmech143@gmail.com>
2024-02-07 12:11:06 +0530

    basic content creation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import InitialReactApp from './pages/InitialReactApp';
+import { Context } from './state/dummy-context';
 
 function App() {
+  const dummyContext = {
+      dummyData: ['ravi', "preethi"]
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        Test
-       <InitialReactApp dataFromParent="SampleData"></InitialReactApp>
-      </header>
-    </div>
+    <Context.Provider value={dummyContext}>
+      <div className="App">
+        <header className="App-header">
+          Test
+          <InitialReactApp dataFromParent="SampleData"></InitialReactApp>
+        </header>
+      </div>
+    </Context.Provider>
   );
 }
 
--- src/pages/InitialReactApp.js
+++ src/pages/InitialReactApp.js
@@ -1,7 +1,17 @@
+import { useContext } from 'react';
 import style from './InitialReactApp.module.css';
+import { Context } from '../state/dummy-context';
 
 function InitialReactApp(props) {
-    return <div className={style.blueColor}> InitialReactApp {props.dataFromParent} </div>
+
+    const dummyContext = useContext(Context);
+
+    return <div className={style.blueColor}>
+        InitialReactApp {props.dataFromParent}
+        <div>
+            {dummyContext.dummyData.map((data, i) => <div key={i}>{data}</div>)}
+        </div>
+    </div>
 }
 
 export default InitialReactApp;
